ceda38b9ffb0616f2431f37ab1888564506fb4d3
zhangyipeng <zhangyipeng91@outlook.com>
2019-11-08 23:03:17 +0800

    fix bug

--- src/environment.cpp
+++ src/environment.cpp
@@ -77,9 +77,10 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer)
   ProcessPointClouds<pcl::PointXYZI> pointProcessor;
   pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud = pointProcessor.loadPcd("../src/sensors/data/pcd/data_1/0000000000.pcd");
   pcl::PointCloud<pcl::PointXYZI>::Ptr filterCloud = pointProcessor.FilterCloud(inputCloud,0.28,Eigen::Vector4f (-8,-6,-8,1),Eigen::Vector4f (15,8,8,1));
+  std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor.SegmentPlane(filterCloud, 100, 0.2);
 
   //renderPointCloud(viewer,inputCloud,"inputCloud");
-  renderPointCloud(viewer,filterCloud,"filterCloud");
+  renderPointCloud(viewer,segmentCloud,"segmentCloud");
 
 }
 
